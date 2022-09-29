0a2e7c07b517166b6b43519c3072b3be66cfc00e
unknown <omjtommie>
2022-09-29 18:04:31 +0100

    Footer-made

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,12 +1,15 @@
-import './Assets/styles/App.css';
-import Navbar from './layouts/Navbar/Navbar';
+import "./Assets/styles/App.css";
+import Navbar from "./layouts/Navbar/Navbar";
+import Footer from "./layouts/Footer/Footer";
 
 // first layer of Routing is done here!
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar />
-    </>
+   
+      <Footer />
+    </div>
   );
 }
 
---
+++ web-app/src/layouts/Footer/Footer.jsx
@@ -0,0 +1,69 @@
+// import React from 'react';
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+        <div className="footer-logo">
+          <h1>
+            <span style={{ color: "#ffff" }}>Eat</span>
+            <span style={{ color: "#FF8323" }}>Hub</span>
+          </h1>
+          
+        </div>
+          <div className="footer-col">
+            <h4>Support</h4>
+            <ul>
+              <li>
+                <a href="account">Account</a>
+              </li>
+              <li>
+                <a href="support">Support Center</a>
+              </li>
+              <li>
+                <a href="feed">Feedback</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Links</h4>
+            <ul>
+              <li>
+                <a href="about">About Us</a>
+              </li>
+              <li>
+                <a href="privacy">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="terms">Terms of Service</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Get Help</h4>
+            <ul>
+              <li>
+                <a href="add">Adding your restuarant</a>
+              </li>
+              <li>
+                <a href="delivery">Joining our delivery service</a>
+              </li>
+              <li>
+                <a href="kitchen">Starting Your Kitchen</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-bottom-text">
+          <p>2022 Eathub</p>
+          <p>eathublagos@gmail.com</p>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
