5ff4c821436c8f7cadb5997e4a9ef87c20e87b67
Olaoluwa Jesutomisin <omjtomisin@gmail.com>
2022-10-03 02:15:53 +0100

    changes in appjs

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -5,11 +5,11 @@ import Footer from "./layouts/Footer/Footer";
 // first layer of Routing is done here!
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
    
       <Footer />
-    </div>
+    </>
   );
 }
 
