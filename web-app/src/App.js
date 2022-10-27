39b317e1e41f1cec898658dd571c3f86b9a1e167
Daniel Abati <danielabatibabatunde1@gmail.com>
2022-10-27 18:03:23 +0100

    Dashboard

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,6 +1,6 @@
 import './Assets/styles/App.css';
 import Homepage from './Pages/Homepage';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -8,32 +8,23 @@ import {
 } from "react-router-dom";
 
 import Formpage from './Pages/form/Formpage';
-import DashboardPage from './Pages/dashboard/DashboardPage';
-import OrdersPage from './Pages/orders/OrdersPage';
-import MenuPage from './Pages/menu/MenuPage';
-import AnalyticsPage from './Pages/analytics/AnalyticsPage';
-import MessagePage from './Pages/message/MessagePage';
-import SettingsPage from './Pages/settiings/SettingsPage';
-import PayoutPage from './Pages/payouts/PayoutPage';
+import BusinessForm from './Components/BusinessForm/BusinessForm';
+import PersonalForm from './Components/PersonalForm/PersonalForm';
 
 
 // first layer of Routing is done here!
 class App extends React.Component {
+
   render() {
     return (
       <BrowserRouter>
       
         <Routes>
           <Route exact path="/" element={<Homepage />} />
-          <Route path="signup" element={<Formpage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="orders" element={<OrdersPage />} />
-          <Route path="menu" element={<MenuPage />} />
-          <Route path="analytics" element={<AnalyticsPage />} />
-          <Route path="message" element={<MessagePage />} />
-          <Route path="settings" element={<SettingsPage />} />
-          <Route path="payout" element={<PayoutPage />} />
-          
+          <Route path="signup" element={<Formpage />}>
+            <Route index element={<PersonalForm />} />
+            <Route path="business" element={<BusinessForm />} />
+          </Route>
         </Routes>
         
       </BrowserRouter>
