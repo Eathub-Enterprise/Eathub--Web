63737828b60951f48659dafffa3d09f3d4e10fdd
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-08-09 22:18:38 +0100

    added custom hook

---
+++ web-app/src/Hooks/useDataFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useDataFetch = (url, options = {}) => {
+  // set the states
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // do the data fetching here
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url, options);
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return { data, loading, error };
+};
+
+export default useDataFetch;
