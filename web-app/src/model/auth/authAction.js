1496a5670262b20bc698b2e03e3e28dcb2bd8560
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-09-30 23:13:40 +0100

    attempt to write test

--- web-app/src/model/auth/authAction.js
+++ web-app/src/model/auth/authAction.js
@@ -38,7 +38,6 @@ export const vendorLogin = createAsyncThunk(
         config
       );
       localStorage.setItem("vendor", JSON.stringify(data));
-      console.log(data);
       return data;
     } catch (error) {
       if (error.response && error.response.data.message) {
