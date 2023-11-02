a49cc5ac0f4f49229f91f832e5112d81d163642a
Olaoluwa Jesutomisin <omjtomisin@gmail.com>
2023-11-02 18:37:23 +0100

    links fix

--- web-app/src/Pages/misc/Links/Terms.jsx
+++ web-app/src/Pages/misc/Links/Terms.jsx
@@ -21,25 +21,31 @@ const Terms = () => {
             shall be effective after duly signed or sealed by the Parties. B.
             Upon consent as provided above, these Client Terms and Conditions
             ("Terms") are an agreement between the Client' Business and Eathub
-            and the Terms goes : 1. Eathub is responsible for the availability
+            and the Terms goes :
+            <br></br>1. Eathub is responsible for the availability
             of service and the technology needed by the client. Services such as
             order processing, marketing, advertising and promotional services,
             proprietary information services, onboarding, operational and other
-            support services. 2. Eathub is to provide technological tools such
-            as website, mobile application and other technological interface. 3.
-            The Proposed Client is to have the option to access different
+            support services.
+            <br></br> 2. Eathub is to provide technological tools such
+            as website, mobile application and other technological interface. 
+            <br></br>3.The Proposed Client is to have the option to access different
             categories of sales channels : - Auto Service : Eathub, responsible
             for the full service from package pickup to delivery and other
             operations. - Self Service : The Proposed Client is responsible for
-            Food logistics (Food delivery), and customer service. 4. The
+            Food logistics (Food delivery), and customer service.
+            <br></br> 4. The
             Proposed Client is to make available location operated and owned by
-            the client service to Eathub. 5. The Proposed Client is to make
-            available it's information to be verified at intervals to Eathub. 6.
-            Also, Eathub is here to provide a technology that connects customers
-            to the Proposed client's business. 7. The Proposed Client however,
+            the client service to Eathub.
+            <br></br> 5. The Proposed Client is to make
+            available it's information to be verified at intervals to Eathub.
+            <br></br>6. Also, Eathub is here to provide a technology that connects customers
+            to the Proposed client's business.
+            <br></br> 7. The Proposed Client however,
             is not forbidden from accessing the information regarding the
             customers using the platform for marketing and customer retention
-            purposes which is the responsibility of Eathub. 8. Clients however
+            purposes which is the responsibility of Eathub.
+            <br></br> 8. Clients however
             can advertise their meal packages or sweet deals on the platform at
             an additional fee as the algorithm is responsible for displaying
             these packages to the end-user based on merit.
