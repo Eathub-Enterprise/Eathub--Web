bb4487212baebf66f030872801e358f28e51da2d
Daniel Abati <danielabatibabatunde1@gmail.com>
2024-02-23 05:57:08 +0100

    fixed the ui and state of web-app

--- web-app/src/Pages/dashboard/pages/menu/EmptyMenu.jsx
+++ web-app/src/Pages/dashboard/pages/menu/EmptyMenu.jsx
@@ -3,20 +3,19 @@ import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
 import animationData from "../../../../Assets/lotties/5081-empty-box.json";
 
-
 const EmptyMenu = () => {
-    const defaultOptions = {
-        loop: false,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice",
-        },
-      };
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div className="menu-section">
-      <span className="empty-menu" style={{marginTop: 50}}>
-      <Lottie options={defaultOptions} height={400} width={400} />
+      <span className="empty-menu">
+        <Lottie options={defaultOptions} height={450} width={450} />
         <h1>Your Menu List is currently Empty!</h1>
         <button className="menu-btn">
           <Link to="/dashboard/menu/addMenu" className="menu-link">
